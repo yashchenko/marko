e22b8269b581db03db7839dc15fbbc6a1862830f
Ivan Yashchenko <ivan.profitx@gmail.com>
2025-07-22 15:34:30 +0300

    feat(KAN-2): Refactor image loading to use Kingfisher

--- Marko/Features/Teacher/Model/Teacher.swift
+++ Marko/Features/Teacher/Model/Teacher.swift
@@ -8,23 +8,21 @@
 import UIKit
 
 struct Teacher {
-    let id: String // Firestore Document ID
+    let id: String
     let name: String
     let subject: String
     let description: String
     let rank: String
     let profileImageURL: String
-    var profileImage: UIImage // Keep the downloaded image
 
-    // Initializer provides a default placeholder image
-    // This default is used if the image download fails or before it completes.
-    init(id: String, name: String, subject: String, description: String, rank: String, profileImageURL: String, profileImage: UIImage = UIImage(systemName: "person.crop.circle.fill")!) {
+    // The initializer no longer deals with UIImages at all.
+    // It just takes the data.
+    init(id: String, name: String, subject: String, description: String, rank: String, profileImageURL: String) {
         self.id = id
         self.name = name
         self.subject = subject
         self.description = description
         self.rank = rank
         self.profileImageURL = profileImageURL
-        self.profileImage = profileImage
     }
 }
